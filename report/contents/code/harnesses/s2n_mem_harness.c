ef8c966683b6bb2a9abbdd8481569d010ee65193
Issac Lam <aryuen0805@gamil.com>
2016-08-25 05:02:59 +0100

    update the report

---
+++ report/contents/code/harnesses/s2n_mem_harness.c
@@ -0,0 +1,21 @@
+#include "error/s2n_errno.h"
+#include "utils/s2n_safety.h"
+
+#include "utils/s2n_mem.h"
+#include "utils/s2n_blob.h"
+
+uint32_t nondet_uint32();
+
+int main(int argc, char **argv)
+{
+    uint32_t blob_size = nondet_uint32();
+	__CPROVER_assume(blob_size > 0);
+
+    struct s2n_blob b;
+    
+    GUARD(s2n_mem_init());
+	GUARD(s2n_alloc(&b, blob_size));
+	GUARD(s2n_realloc(&b, nondet_uint32()));
+	GUARD(s2n_free(&b));
+	return 0;
+}
